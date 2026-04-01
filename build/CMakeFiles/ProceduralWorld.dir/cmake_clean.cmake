fa2aca87a26a39699bf9f62c31cd09d43d385afd
ummjanavi <jmunagav@purdue.edu>
2026-04-01 18:48:21 -0400

    update WorldData.h it compiles now

---
+++ build/CMakeFiles/ProceduralWorld.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProceduralWorld.dir/src/core/Window.cpp.o"
+  "CMakeFiles/ProceduralWorld.dir/src/core/Window.cpp.o.d"
+  "CMakeFiles/ProceduralWorld.dir/src/main.cpp.o"
+  "CMakeFiles/ProceduralWorld.dir/src/main.cpp.o.d"
+  "CMakeFiles/ProceduralWorld.dir/src/raytracer/Renderer.cpp.o"
+  "CMakeFiles/ProceduralWorld.dir/src/raytracer/Renderer.cpp.o.d"
+  "CMakeFiles/ProceduralWorld.dir/src/terrain/HeightMap.cpp.o"
+  "CMakeFiles/ProceduralWorld.dir/src/terrain/HeightMap.cpp.o.d"
+  "ProceduralWorld"
+  "ProceduralWorld.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProceduralWorld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
