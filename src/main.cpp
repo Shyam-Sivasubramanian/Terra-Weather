3466d57640a48553eeddb521b3b77f88775fd991
Shyam Sivasubramanian <shyam.siva@outlook.com>
2026-04-17 13:23:48 -0400

    ff

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,6 @@
 #include "WeatherMap.h"
 #include "Atmosphere.h"
 
-<<<<<<< HEAD
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include <memory>
@@ -214,36 +213,3 @@ int main() {
 
     return 0;
 }
-=======
-#include <iostream>
-#include "WorldData.h"
-#include "Window.h"
-#include "HeightMap.h"
-#include "Renderer.h"
-
-int main() {
-    Window window;
-    if (!window.init(1280, 720, "ProceduralWorld")) {
-        return 1;
-    }
-
-    WorldData worldData;
-    worldData.width = 256;
-    worldData.height = 256;
-    worldData.seed = 42;
-
-    HeightMap::build(worldData);
-
-    std::cout << "Height map generated with "
-              << worldData.heightMap.size()
-              << " cells.\n";
-
-    while (!window.shouldClose()) {
-        window.pollEvents();
-        Renderer::renderStub(worldData);
-        window.swapBuffers();
-    }
-
-    return 0;
-}
->>>>>>> fa2aca87a26a39699bf9f62c31cd09d43d385afd
